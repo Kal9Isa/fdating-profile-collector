40098cc6cb540aa91c723295bf9d9f3e8de48cda
kal9isa <docalisa_it@yahoo.com>
2021-08-25 23:35:19 +0430

    get filter params from user

---
+++ master/src/index.ts
@@ -0,0 +1,10 @@
+// fdating.com
+import axios from 'axios';
+
+import { getParams } from './services/get-params';
+
+const baseUrl: string = 'https://fdating.com';
+const searchUrl: string = 'https://fdating.com/search?do=Search;';
+
+const userParams = getParams(process.argv.slice(2));
+console.log(userParams);
---
+++ master/src/services/get-params.ts
@@ -0,0 +1,22 @@
+import minimist from 'minimist';
+
+interface fdatingParams {
+  gender: number;
+  startAge: number;
+  endAge: number;
+  photo: boolean;
+}
+
+export const getParams = (args: string[]): fdatingParams => {
+  const parsedArgs = minimist(args);
+  if (!!!parsedArgs.g) parsedArgs.g = 0;
+  if (!!!parsedArgs.s) parsedArgs.s = 18;
+  if (!!!parsedArgs.e) parsedArgs.e = 99;
+  if (!!!parsedArgs.p) parsedArgs.p = true;
+  return {
+    gender: parsedArgs.g,
+    startAge: parsedArgs.s,
+    endAge: parsedArgs.e,
+    photo: parsedArgs.p,
+  };
+};
