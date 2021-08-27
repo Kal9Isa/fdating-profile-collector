226d046451d661774ed55d670ad192529928a5e1
kal9isa <docalisa_it@yahoo.com>
2021-08-27 22:42:58 +0430

    change output of compare fn

--- master/src/services/compare-filters.ts
+++ master/src/services/compare-filters.ts
@@ -5,17 +5,17 @@ const filtersKey = 'filters';
 
 export const compareFilters = async (
   filters: fdatingParams
-): Promise<fdatingParams> => {
+): Promise<boolean> => {
   let savedFilters = await redisClient.aGet(filtersKey);
   if (Object.keys(savedFilters).length === 0) {
     await redisClient.aSet(filtersKey, JSON.stringify(filters));
-    return savedFilters;
+    return false;
   } else {
     const newFilters = JSON.stringify(filters);
-    if (JSON.stringify(savedFilters) === newFilters) return savedFilters;
+    if (JSON.stringify(savedFilters) === newFilters) return true;
     else {
       await redisClient.aSet(filtersKey, newFilters);
-      return filters;
+      return false;
     }
   }
 };
