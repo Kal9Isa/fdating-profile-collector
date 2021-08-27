cf9cd99ff21ebbc46d0d43a3a11a600e418e9426
kal9isa <docalisa_it@yahoo.com>
2021-08-27 17:26:58 +0430

    redis interface to async the reqs

---
+++ master/src/interfaces/redis-connector.ts
@@ -0,0 +1,35 @@
+import redis, { RedisClient } from 'redis';
+import { promisify } from 'util';
+
+interface KVClient {
+  aGet(key: string): Promise<object>;
+  aSet(key: string, value: string): Promise<void>;
+}
+
+class RedisInterface implements KVClient {
+  client: RedisClient = redis.createClient('redis://194.5.207.227:32200');
+
+  constructor() {}
+
+  async aGet(key: string) {
+    const result = await promisify(this.client.get)
+      .bind(this.client)(key)
+      .then((res: string | null) => {
+        if (res) return JSON.parse(res);
+        else return {};
+      })
+      .catch((err: string) => console.error(err));
+    return result;
+  }
+
+  async aSet(key: string, value: string) {
+    await promisify(this.client.set)
+      .bind(this.client)(key, value)
+      .then((res) => console.log(res))
+      .catch((err: string) => console.error(err));
+  }
+}
+
+const redisClient = new RedisInterface();
+
+export { redisClient };
