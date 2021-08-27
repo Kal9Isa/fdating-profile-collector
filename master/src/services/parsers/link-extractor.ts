aa78ad296b8e624af7ae5e9d25c4098ff7040d1c
kal9isa <docalisa_it@yahoo.com>
2021-08-27 22:08:12 +0430

    link extractor done

--- master/src/services/parsers/link-extractor.ts
+++ master/src/services/parsers/link-extractor.ts
@@ -1,15 +1,17 @@
 import { AxiosResponse } from 'axios';
 import cheerio from 'cheerio';
 
-export const linkExtractor = (payload: AxiosResponse): string[] => {
-  let profileLinks: string[] = [];
+export const linkExtractor = (payload: AxiosResponse): object => {
+  // let profileLinks: string[] = [];
+  let profileLinks = {};
   const html = payload!.data;
   const $ = cheerio.load(html);
   const linkList = $('.c-block > .inner > center > ul > li > .image');
 
   for (const item of linkList!) {
     const link = $(item).attr('href');
-    profileLinks.push(link!);
+    // profileLinks.push(link!);
+    profileLinks[link.match(/\d+/)[0]] = link;
   }
 
   return profileLinks;
