73bbd65f62976e640841069d8722e339d3c41580
kal9isa <docalisa_it@yahoo.com>
2021-08-27 21:31:38 +0430

    shape extracted data into object

--- master/src/services/parsers/link-extractor.ts
+++ master/src/services/parsers/link-extractor.ts
@@ -2,16 +2,19 @@ import { AxiosResponse } from 'axios';
 import cheerio from 'cheerio';
 
 export const linkExtractor = (payload: AxiosResponse | void): void => {
+  let profileLinks = {};
   if (payload) {
     const html = payload.data;
     const $ = cheerio.load(html);
     const linkList = $('.c-block > .inner > center > ul > li > .image');
 
-    console.log(linkList);
     if (linkList) {
       for (const item of linkList) {
-        console.log($(item).attr('href'));
+        const link = $(item).attr('href');
+        profileLinks[link.match(/\d+/)[0]] = link;
       }
-    } else console.log('nothing');
+
+      console.log(profileLinks);
+    }
   }
 };
