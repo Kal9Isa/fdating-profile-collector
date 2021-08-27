c24f56f7e25816ac17f795e795170b2b93444d71
kal9isa <docalisa_it@yahoo.com>
2021-08-27 23:45:43 +0430

    minor fix

--- master/src/index.ts
+++ master/src/index.ts
@@ -2,13 +2,6 @@
 
 import { compareFilters } from './services/compare-filters';
 import { getParams } from './services/get-params';
-import { searchSite } from './services/search-site';
-import {
-  searchPageParser,
-  SearchInfo,
-} from './services/parsers/search-page-parser';
-import { saveSearchInfo } from './services/save-search-info';
-import { linkExtractor } from './services/parsers/link-extractor';
 import { linkCollector } from './services/link-collector';
 
 const baseUrl: string = 'https://fdating.com';
