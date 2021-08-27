8b2fe4ae6f54fb71967ab52c67844763a75d778b
kal9isa <docalisa_it@yahoo.com>
2021-08-27 21:20:49 +0430

    extracting profile links test

--- master/src/services/parsers/link-extractor.ts
+++ master/src/services/parsers/link-extractor.ts
@@ -5,16 +5,12 @@ export const linkExtractor = (payload: AxiosResponse | void): void => {
   if (payload) {
     const html = payload.data;
     const $ = cheerio.load(html);
-    const linkList = $('.c-block > .inner > center > ul > li > .image').attr(
-      'href'
-    );
+    const linkList = $('.c-block > .inner > center > ul > li');
 
     console.log(linkList);
     if (linkList) {
-      console.log(linkList);
-
       for (const item of linkList) {
-        console.log(item);
+        console.log($(item).attr('href'));
       }
     } else console.log('nothing');
   }
