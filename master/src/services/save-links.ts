3b6b56f980e337feed504b60404e7f81e77d43a1
kal9isa <docalisa_it@yahoo.com>
2021-08-28 23:21:56 +0430

    added connection to events & add new eventhandler

--- master/src/services/save-links.ts
+++ master/src/services/save-links.ts
@@ -16,17 +16,25 @@ const profileQueue: Queue = new Queue('getProfiles', {
   },
 });
 
-const queueEvents = new QueueEvents('getProfiles');
+const queueEvents: QueueEvents = new QueueEvents('getProfiles', {
+  connection: {
+    host: '194.5.207.227',
+    port: 32200,
+  },
+});
 
 queueEvents.on('completed', (jobId: string) => {
   console.log(`profile ${jobId} retrieved`);
 });
 
+queueEvents.on('added', (jobId: string) => {
+  console.log(`saving ${jobId} as a job`);
+});
+
 export const saveLinks = async (links: object): Promise<void> => {
   Object.keys(links).forEach(async (key) => {
-    let job = await profileQueue.add(key, links, {
+    await profileQueue.add(key, links, {
       jobId: key,
     });
-    console.log(`saving ${job.id} as a job`);
   });
 };
