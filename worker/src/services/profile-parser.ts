48c3369c030fe6ae89bf07243277eb9ddb3b3486
kal9isa <docalisa_it@yahoo.com>
2021-08-30 00:06:43 +0430

    examine profile table

--- worker/src/services/profile-parser.ts
+++ worker/src/services/profile-parser.ts
@@ -1,8 +1,31 @@
 import { AxiosResponse } from 'axios';
 import cheerio from 'cheerio';
 
+interface UserProfile {
+  profileId: string;
+  registered: string;
+  updated: string;
+  lastLogged: string;
+  name: string;
+  country: string;
+  state: string;
+  city: string;
+  maritalStatus: string;
+  age: string;
+  zodiac: string;
+  height: string;
+  weight: string;
+  hairColor: string;
+  eyeColor: string;
+  children: string;
+  smoking: string;
+  drinking: string;
+  languages: string;
+}
+
 // Parse profile data returned by axios
 export const profileParser = (payload: AxiosResponse) => {
+  let user = {};
   const html = payload!.data;
   const $ = cheerio.load(html);
   const profileTable = $('.profile-tbl > tbody > tr');
@@ -10,6 +33,7 @@ export const profileParser = (payload: AxiosResponse) => {
   for (const item of profileTable) {
     const key = $(item).find('th').text().trim();
     const value = $(item).find('td').text().trim();
-    console.log(`${key}: ${value}`);
+    user[key] = value;
   }
+  console.log(JSON.stringify(user));
 };
