804241541c53cc1963dc8eb6d2cc7a80180d084f
kal9isa <docalisa_it@yahoo.com>
2021-08-29 22:28:29 +0430

    testing job

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,31 +1,5 @@
-import { Worker } from 'bullmq';
-import path from 'path';
+import { Worker, Job } from 'bullmq';
 
-const main = (): void => {
-  const processorFile = path.join(__dirname, 'processor.ts');
-  const worker: Worker = new Worker('getProfiles', processorFile, {
-    connection: {
-      host: '194.5.207.227',
-      port: 32200,
-    },
-    limiter: {
-      max: 5,
-      duration: 1000,
-    },
-  });
-
-  worker.on('drained', async () => {
-    console.log('No jobs left');
-    await worker.close();
-  });
-
-  // worker.on('error', (err) => {
-  //   console.error(err);
-  // });
-
-  process.on('SIGINT', async () => {
-    await worker.close();
-  });
-};
-
-main();
+const worker = new Worker('test', async (job: Job) => {
+  console.log('doing job');
+});
--- worker/src/processor.ts
+++
@@ -1,6 +0,0 @@
-import { SandboxedJob } from 'bullmq';
-
-export const processor = async (job: SandboxedJob) => {
-  const baseUrl: string = 'https://fdating.com';
-  console.log(`doing ${baseUrl}${job.data}`);
-};
