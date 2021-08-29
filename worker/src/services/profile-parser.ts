f21fafb0f7e125104d4d03da610c4bc8191b513e
kal9isa <docalisa_it@yahoo.com>
2021-08-30 00:19:47 +0430

    extracting name

--- worker/src/services/profile-parser.ts
+++ worker/src/services/profile-parser.ts
@@ -38,7 +38,7 @@ export const profileParser = (payload: AxiosResponse) => {
 
   // TODO extract photo link
   // TODO extract name
-  const userName = $('.user-page').find('#text').text();
+  const userName = $('.user-page').find('h4').text();
 
   console.log(userName);
 };
