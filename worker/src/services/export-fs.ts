5d474bb3f8c5dd3c1580c6dfb0ba61eaf6c522d3
kal9isa <docalisa_it@yahoo.com>
2021-08-30 00:54:15 +0430

    change dir for data

--- worker/src/services/export-fs.ts
+++ worker/src/services/export-fs.ts
@@ -2,7 +2,7 @@ import { UserProfile } from '../model/User';
 import * as fs from 'fs-extra';
 
 export const exportFS = async (user: UserProfile): Promise<void> => {
-  fs.outputFile(`${user.profileId}/info.json`, JSON.stringify(user))
+  fs.outputFile(`../../data/${user.profileId}/info.json`, JSON.stringify(user))
     .then(() => {
       console.log(`getting image from ${user.image}`);
     })
